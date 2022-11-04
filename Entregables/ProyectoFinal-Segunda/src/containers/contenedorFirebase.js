e66d6231eaf0c67111b3dfd48caaacefa7fa503f
CordobaFran <francocordobafs@gmail.com>
2022-11-03 23:06:44 -0300

    first commit

---
+++ Entregables/ProyectoFinal-Segunda/src/containers/contenedorFirebase.js
@@ -0,0 +1,78 @@
+import admin from 'firebase-admin'
+import config from '../config.js';
+
+admin.initializeApp({
+  credential: admin.credential.cert(config.firebase)
+});
+
+const db = admin.firestore();
+
+export default class ContainerFirebase {
+
+  constructor(collection) {
+    this.query = db.collection(collection)
+  }
+
+  //Create document
+  async create() {
+    try {
+      const doc = this.query.doc()
+      await doc.create({
+        nombre: "juan",
+        apellido: "perez"
+      })
+      console.log("creado");
+    } catch (error) {
+      console.log(error);
+    }
+  }
+  //Read All
+  async getAll() {
+    try {
+      const queryDocs = await this.query.get()
+      let docs = queryDocs.docs.map(documents => (
+        { id: documents.id, ...documents.data() }
+      ))
+      console.log(docs);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+  //Read by ID
+  async getById(id) {
+    try {
+      const queryDoc = this.query.doc(id)
+      const doc = await queryDoc.get()
+      const document = doc.data()
+      console.log(document);
+
+    } catch (error) {
+      console.log(error);
+    }
+  }
+  //Update Docs
+  async update(id) {
+    try {
+      const doc = this.query.doc(id)
+      const update = await doc.update({
+        nombre: 'Franco'
+      })
+      console.log(`documento actualizado`);
+
+    } catch (error) {
+      console.log(error);
+    }
+  }
+  //Update Docs
+  async delete(id) {
+    try {
+      const doc = this.query.doc(id)
+      const deleteDoc = await doc.delete()
+      console.log(`documento eliminado`);
+
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+}
---
+++ Entregables/ProyectoFinal-Segunda/src/daos/productos/productosDaoArchivo.js
@@ -0,0 +1,8 @@
+import ContenedorArchivo from "../../containers/contenedorArchivo";
+
+export default class ProductosDaoArchivo extends ContenedorArchivo {
+    constructor(){
+        super("db/productos.json")
+    }
+}
+
